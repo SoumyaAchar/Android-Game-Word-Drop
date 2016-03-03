c15fda0f69f961f27bfb95913bd5ca6d93695fc9
SoumyaAchar <smachar4@gmail.com>
2016-03-03 16:22:56 -0500

    Added files via upload

---
+++ src/main/java/se/android/worddrop/Grid.java
@@ -0,0 +1,56 @@
+package se.android.worddrop;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class Grid {
+    public Tile[][] tile;
+    private Tile t=new Tile();
+
+    public Grid(int rows,int columns){
+        tile=new Tile[rows][columns];
+    }
+
+    public void setTile(Tile[][] tile)
+    {
+        this.tile = tile;
+    }
+    public Tile[][] getTile(){
+        return tile;
+    }
+    public Tile[][] populateGrid(){
+        for(int i=0;i<tile.length;i++){
+            for(int j=0;j<tile[i].length;j++){
+                tile[i][j]=new Tile(t.getRandomLetter(),t.getRandomColor());
+            }
+        }
+        return tile;
+    }
+
+    public Tile[][] refillGrid(Set<String> btnIdsSwipped){
+        if(tile!=null){
+
+            List<String> lst=new ArrayList<String>();
+            lst.addAll(btnIdsSwipped);
+            Collections.sort(lst);
+            Iterator<String> it=lst.iterator();
+            while(it. hasNext()){
+                String btnId=it.next();
+                int len=btnId.length();
+                int x=Character.getNumericValue(btnId.charAt(len-2));
+                int y=Character.getNumericValue(btnId.charAt(len-1));
+                int i;
+                for(i=x;i>0;i--){
+                    tile[i][y]=tile[i-1][y];
+                    String id="button"+i+y;
+                    System.out.println("Button id in grid: "+id);
+                }
+                tile[i][y]=new Tile(t.getRandomLetter(),t.getRandomColor());
+            }
+        }
+        return tile;
+    }
+}
